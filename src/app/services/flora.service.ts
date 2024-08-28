45a886b183440198952cd29dcb35957b30357b55
I4MGlitch <91672477+I4MGlitch@users.noreply.github.com>
2024-08-28 12:43:14 +0800

    1

--- src/app/services/flora.service.ts
+++ src/app/services/flora.service.ts
@@ -1,14 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FloraService {
 
-  private baseUrl = environment.apiUrl;
+  private baseUrl = "http://localhost:3000/api";
 
   constructor(private http: HttpClient) {}
 
